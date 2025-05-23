ad39f0ccf9e591994978b4bbb0a2c8b5f6ccd242
Wilson <wilsonwah123@gmail.com>
2025-05-23 11:12:42 +0800

    Convert journey.jsx to supabase

--- frontend/src/pages/Journey.jsx
+++ frontend/src/pages/Journey.jsx
@@ -6,9 +6,14 @@ import supabase from "../../config/supabaseClient";
 import { useEffect, useState } from "react";
 
 export default function Journey() {
+  const userId = "demo_user"
   const navigate = useNavigate();
 
   const [projects, setProjects] = useState([])
+  const [tasks, setTasks] = useState([])
+  const [projectTasks, setProjectTasks] = useState([])
+  const [taskIds, setTaskIds] = useState([])
+  const [ inboxHistory, setInboxHistory] = useState([])
 
   const fetchProjects = async () => {
     const {error, data} = await supabase
@@ -24,39 +29,111 @@ export default function Journey() {
       setProjects(data)
   };
 
-  useEffect(() => {
-    fetchProjects();
-  }, []);
+  const fetchTasks = async () => {
+    const {error, data} = await supabase
+      .from("tasks")
+      .select("*")
+      .order("id", {ascending: true});
 
-  console.log(projects)
+      if (error) {
+        console.error("Error reading tasks: ", error.message);
+        return
+      }
+      
+      setTasks(data)
+  };
+    
+  const fetchUserState = async () => {
+    const {error, data} = await supabase
+      .from("user_state")
+      .select("*")
+
+      if (error) {
+        console.error("Error reading user state: ", error.message);
+        return
+      }
+      
+      setTasks(data)
+  };
+
+  const fetchTaskIds = async () => {
+    const {data, error } = await supabase
+        .from("tasks")
+        .select("id")
+
+      if (error) {
+        console.error("Error fetching tasks id's:", error);
+        return
+      }
+
+      setTaskIds(data?.map(task => task.id) || [])
+  }
+
+  const fetchInboxHistory = async () => {
+    const { data, error } = await supabase
+        .from("user_state")
+        .select("inbox_history")
+        .eq("user_id", userId)
+        .single();
+
+      if (error) {
+        console.error("Error fetching inbox_history:", error);
+        return
+      }
+
+      setInboxHistory(data?.inbox_history || []);
+  }
+
+  useEffect(() => {
+    fetchUserState();
+    fetchTasks();
+    fetchProjects();
+    fetchTaskIds();
+    fetchInboxHistory();
+  }, []);
 
   const handleSelect = async (id) => {
     const { error } = await supabase
       .from("user_state")
-      .update({ current_project: id, shipped_tasks: {} })
+      .update( { selected_project_id: id } )
+      .eq("user_id", userId )
+
+      if(error){
+        console.error("Error Selecting Project:", error)
+      } else {
+
+        const firstTaskId = Number(`${id}01`);
+
+        if (!inboxHistory.includes(firstTaskId)) {
+          
+          const { data, error } = await supabase
+          .from("tasks")
+          .select("*")
+          .eq("id", firstTaskId)
+          .single();
+          
+          if (error) {
+            console.error("Error fetching first task:", error);
+            return
+          } else {
+            const updatedHistory = [...inboxHistory, firstTaskId]
+
+            const {error2} = await supabase
+              .from("user_state")
+              .update({current_task: data, inbox_history: updatedHistory, task_just_shipped: true })
+              .eq("user_id", userId)
+
+              if(error2){
+                console.error("Error Updating:", error)
+              }
+          } 
+        }
+      }
+
+    
 
     navigate("/inbox")
   }
-
-  // const handleSelect = (id) => {
-  //   localStorage.setItem('selectedProjectId', id);
-
-  //   // Check if this project already has inbox history
-  //   const historyKey = `inboxHistory_${id}`;
-  //   const inboxHistory = JSON.parse(localStorage.getItem(historyKey)) || [];
-
-  //   // If no task was shipped yet, add the first one
-  //   const selectedProject = projects.find((proj) => proj.id === id);
-  //   if (inboxHistory.length === 0 && selectedProject?.tasks.length > 0) {
-  //     const firstTask = selectedProject.tasks[0];
-  //     localStorage.setItem(historyKey, JSON.stringify([firstTask.id]));
-  //     localStorage.setItem('currentTask', JSON.stringify(firstTask));
-  //   }
-
-  //   localStorage.setItem("taskJustShipped", "true");
-  //   window.location.href = '/inbox';
-  // };
-
 
 
 
