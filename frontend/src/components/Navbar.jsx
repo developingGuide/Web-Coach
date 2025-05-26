83583e85d204c4f4c7cdd3051565e034db5db8e2
Wilson <wilsonwah123@gmail.com>
2025-05-26 20:56:17 +0800

    Looks nice as a UI Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,23 +1,55 @@
-// components/Navbar.js
-import React from 'react';
-import './Navbar.css'; // We'll create this for styling
+// // components/Navbar.js
+// import React from 'react';
+// import './Navbar.css'; // We'll create this for styling
 
-const Navbar = ({ exp = 45, maxExp = 100 }) => {
+// const Navbar = ({ exp = 45, maxExp = 100 }) => {
+//   const progressPercent = (exp / maxExp) * 100;
+
+//   return (
+//     <div className="navbar">
+//       <img src="../public/Logo.png" alt="Logo" className="navbar-logo" />
+
+//       <div className="exp-bar-container">
+//         <div className="exp-bar">
+//           <div className="exp-fill" style={{ width: `${progressPercent}%` }}></div>
+//         </div>
+//         <span className="exp-text">{exp} / {maxExp} EXP</span>
+//       </div>
+
+//       <div className="profile-circle">
+//         <span>W</span> {/* You can replace with profile image */}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Navbar;
+
+
+// components/Navbar.js
+import './Navbar.css';
+
+const Navbar = ({ exp = 45, maxExp = 100, level = 3 }) => {
   const progressPercent = (exp / maxExp) * 100;
 
   return (
     <div className="navbar">
       <img src="../public/Logo.png" alt="Logo" className="navbar-logo" />
 
-      <div className="exp-bar-container">
-        <div className="exp-bar">
-          <div className="exp-fill" style={{ width: `${progressPercent}%` }}></div>
+      <div className="navbar-right">
+        <div className="profile-circle large">
+          <span>W</span>
         </div>
-        <span className="exp-text">{exp} / {maxExp} EXP</span>
-      </div>
 
-      <div className="profile-circle">
-        <span>W</span> {/* You can replace with profile image */}
+        <div className="exp-info">
+          <div className="exp-label">
+            <span className="level">Lv {level}</span>
+          </div>
+          <div className="exp-bar">
+            <div className="exp-fill" style={{ width: `${progressPercent}%` }}></div>
+          </div>
+          <span className="exp-text">{exp} / {maxExp} EXP</span>
+        </div>
       </div>
     </div>
   );
