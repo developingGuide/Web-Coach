8dcc8845f0bc402bce69c53cf3c6b5294acbd897
Wilson <wilsonwah123@gmail.com>
2025-06-09 20:55:25 +0800

    Auth Updated!

--- frontend/src/components/AuthContext.jsx
+++ frontend/src/components/AuthContext.jsx
@@ -1,4 +1,4 @@
-// AuthContext.js
+// contexts/AuthContext.js
 import { createContext, useEffect, useState } from "react";
 import supabase from "../../config/supabaseClient";
 
@@ -8,20 +8,22 @@ export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
 
   useEffect(() => {
-    const getSession = async () => {
-      const { data: { session } } = await supabase.auth.getSession();
-      console.log(session)
-      setUser(session?.user ?? null);
-    };
-
-    getSession();
-
-    const { data: listener } = supabase.auth.onAuthStateChange((_event, session) => {
-      setUser(session?.user ?? null);
+    const session = supabase.auth.getSession().then(({ data }) => {
+      setUser(data.session?.user || null);
     });
 
-    return () => listener.subscription.unsubscribe();
+    const { data: listener } = supabase.auth.onAuthStateChange((_event, session) => {
+      setUser(session?.user || null);
+    });
+
+    return () => {
+      listener.subscription.unsubscribe();
+    };
   }, []);
 
-  return <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>;
+  return (
+    <AuthContext.Provider value={{ user }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,14 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import { AuthContext } from './components/AuthContext.jsx'
+import { AuthContext, AuthProvider } from './components/AuthContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <AuthContext>
+      <AuthProvider>
         <App />
-      </AuthContext>
+      </AuthProvider>
     </BrowserRouter>
   </StrictMode>,
 )
