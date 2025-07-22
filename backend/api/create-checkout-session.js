374b04a670548a534ba3cf7edecf0368232ea5f8
Wilson <wilsonwah123@gmail.com>
2025-07-22 10:29:49 +0800

    fixing backend error pt 4

--- backend/api/create-checkout-session.js
+++ backend/api/create-checkout-session.js
@@ -5,6 +5,7 @@ import Stripe from 'stripe';
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
+  console.log("🔥 API HIT:", req.method);
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method Not Allowed" });
   }
